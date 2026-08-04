4593c5eea95f42f297d16527f8684ab8d870b1d5
Piano&vinoBar <noesmoscada@gmail.com>
2026-08-03 23:27:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 import { db } from "./firebase.js";
+
 import {
 collection,
-getDocs
+getDocs,
+query,
+where
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 
 async function probarConexion() {
 try {
 const snapshot = await getDocs(collection(db, "entradas"));
+
 console.log("✅ Firebase conectado.");
 console.log("Entradas encontradas:", snapshot.size);
 
-alert("✅ Firebase conectado correctamente");
+const entrada = snapshot.docs[0]?.data();
+
+if (entrada) {
+console.log("Primera entrada:", entrada.codigo);
+alert("Entrada encontrada: " + entrada.codigo);
+}
+
 } catch (error) {
 console.error(error);
 alert("❌ Error al conectar Firebase");
 }
 }
+
 
 probarConexion();
