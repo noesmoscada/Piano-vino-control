3dc1faac0458785eaf305869d79429b79f4951f6
Piano&vinoBar <noesmoscada@gmail.com>
2026-08-04 23:03:36 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,15 @@
-// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-H3YmZbAhZLHZeiDuZmKxwvq6u1epKR8",
-  authDomain: "ticketera2026.firebaseapp.com",
-  projectId: "ticketera2026",
-  storageBucket: "ticketera2026.firebasestorage.app",
-  messagingSenderId: "418520147044",
-  appId: "1:418520147044:web:f0cf5438ae21e87f6beb71"
+apiKey: "AIzaSyDElU_IS5ndsR6e8H66j1GX9RTGVCs5oeU",
+authDomain: "ticketera2026-v2.firebaseapp.com",
+projectId: "ticketera2026-v2",
+storageBucket: "ticketera2026-v2.firebasestorage.app",
+messagingSenderId: "159437918344",
+appId: "1:159437918344:web:052123faa34938fc4a4f3d"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
